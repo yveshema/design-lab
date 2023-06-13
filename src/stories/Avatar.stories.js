13493821f3d12b3bbe80a893d4d753f2b4d46f6a
Yves Rene Shema <rysapptech@gmail.com>
2023-06-12 19:14:20 -0700

    Create a new Avatar story with controls

--- src/stories/Avatar.stories.js
+++ src/stories/Avatar.stories.js
@@ -5,6 +5,18 @@ import { Avatar } from '../components/Avatar';
 export default {
     title: 'Design System/Avatar',
     component: Avatar,
+    /**
+     * More on Storybook argTypes at:
+     * https://storybook.js.org/docs/react/api/argtypes
+     */
+    artTypes: {
+        size: {
+            control: {
+                type: 'select',
+            },
+            options: ['tiny', 'small',' medium', 'large'],
+        },
+    },
 };
 
 export const Standard = (args) => <Avatar {...args} />;
@@ -59,3 +71,22 @@ export const Large = (args) => (
         />
     </div>
 );
+
+/**
+ * New story using Controls
+ * Read more about Storybook templates at:
+ * https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+ */
+const Template = args => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+/**
+ * More on args at:
+ * https://storybook.js.org/docs/react/writing-stories/args
+ */
+Controls.args = {
+    loading: false,
+    size: 'tiny',
+    username: 'Dominic Nguyen',
+    src: 'https://avatars2.githubusercontent.com/u/263385',
+};
